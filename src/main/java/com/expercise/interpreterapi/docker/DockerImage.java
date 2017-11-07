e47a99ab3573eae43cb84124e69c0e685d428f5a
Bay Batu <batuhanbayrakci@gmail.com>
2017-11-07 08:01:55 +0300

    downgrade node version to 8.9.0 lts

--- src/main/java/com/expercise/interpreterapi/docker/DockerImage.java
+++ src/main/java/com/expercise/interpreterapi/docker/DockerImage.java
@@ -2,7 +2,7 @@ package com.expercise.interpreterapi.docker;
 
 public enum DockerImage {
 
-    JAVASCRIPT("node:9.0.0-slim"),
+    JAVASCRIPT("node:8.9.0-slim"),
     PYTHON_2("python:2.7.14-slim-stretch"),
     PYTHON_3("python:3.6.3-slim-stretch");
 
