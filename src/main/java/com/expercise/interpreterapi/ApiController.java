7d703f09bdb3cf7156d500fa1e8688222fe39861
Bay Batu <batuhanbayrakci@gmail.com>
2017-11-12 22:31:53 +0300

    change endpoint because of clashing with swagger-ui.html mapping of spring

--- src/main/java/com/expercise/interpreterapi/ApiController.java
+++ src/main/java/com/expercise/interpreterapi/ApiController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,7 +30,7 @@ public class ApiController {
         this.messageBundle = messageBundle;
     }
 
-    @PostMapping
+    @PostMapping("/eval")
     public InterpretResponse interpret(@Valid @RequestBody InterpretRequest interpretRequest) {
         return interpreterService.interpret(interpretRequest);
     }
